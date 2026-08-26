33efce23d64d0283315526fd7f8cd00cd57f7381
MarecGents <marecgents_tao@sina.com>
2026-08-26 15:38:29 +0800

    版本号修正

--- MGGTMod/MGGTmod.cs
+++ MGGTMod/MGGTmod.cs
@@ -18,7 +18,7 @@ public record ModMetadata : IModMetadata
 	public string Author { get; init; } = "MarecGents";
 	public List<string>? Contributors { get; init; } = ["MarecGents"];
 	public SemanticVersioning.Version Version { get; init; } = new("0.5.2");
-	public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.0");
+	public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.3");
     public bool HasPrepatcher { get; init; } = false;
 	public List<string>? Incompatibilities { get; init; }
 	public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
